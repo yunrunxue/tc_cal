7cc8a98cf6f23fe40a7960f1f7eeb85a5c5c0c94
Your Name <you@example.com>
2017-11-18 11:30:42 +0800

    新[] create n.sh文 file ,creaalysis目 folder structure

---
+++ analysis/mp_tc_t.py
@@ -0,0 +1,5 @@
+import numpy as np 
+f=open('../mp/case1/profile.mp')
+for line in f:
+    print line
+
---
+++ gk/run.sh
@@ -0,0 +1,2 @@
+EXE="/c/Program Files/LAMMPS 64-bit 20160902/bin/lmp_serial.exe"
+"${EXE}" < in.heatflux > out.log
